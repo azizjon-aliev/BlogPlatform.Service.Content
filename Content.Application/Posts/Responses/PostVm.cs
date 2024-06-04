586af4b04d5096f5bf06e805bad3260891e7422c
Azizjon <azizaliev2337@gmail.com>
2024-06-04 18:03:45 +0500

    refactoring

--- Content.Application/Posts/Responses/PostVm.cs
+++ Content.Application/Posts/Responses/PostVm.cs
@@ -1,6 +1,22 @@
+using AutoMapper;
+using BlogPlatform.Service.Common.Mappings;
+using Content.Domain.Entities;
+
 namespace Content.Application.Posts.Responses;
 
-public class PostVm
+public class PostVm : IMapWith<Post>
 {
+    public Guid Id { get; set; }
+    public string Title { get; set; }
     
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Post, PostVm>().ReverseMap()
+            .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
+            .ForMember(d => d.Title, opt => opt.MapFrom(s => s.Title))
+            .ForMember(d => d.CreatedAt, opt => opt.MapFrom(s => s.CreatedAt));
+    }
 }
